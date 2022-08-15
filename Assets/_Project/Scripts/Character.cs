d44d8fc324c8400cb6fdccf7292543fee635e256
busraasevimm <busraasevimm@gmail.com>
2022-08-16 02:31:23 +0300

    dotween & boost

--- Assets/_Project/Scripts/Character.cs
+++ Assets/_Project/Scripts/Character.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public abstract class Character : MonoBehaviour
 {
-   [SerializeField] private Rigidbody _motorbikeRB;
+    [SerializeField] private Rigidbody _motorbikeRB;
     private float _moveSpeed = 10f;
+    private float motorTorque = 1000f;
+    private float boostMultiple = 1f;
     public Transform rotatePoint;
 
     private int _flipCount;
@@ -14,19 +17,23 @@ public abstract class Character : MonoBehaviour
     public WheelCollider fWheel;
     public WheelCollider bWheel;
 
+    private bool onBoost;
+    private float boostTimer;
+    private float defaultBoostTime = 2f;
+    private float plusBoostTime = 1f;
     protected void Construct()
     {
-        
+
     }
 
     internal protected void MoveForward()
     {
-      //  motorbikeRB.transform.position += Vector3.forward * Time.deltaTime * 15f;
-       // _motorbikeRB.velocity = new Vector3(0f, _motorbikeRB.velocity.y, _moveSpeed);
+        //  motorbikeRB.transform.position += Vector3.forward * Time.deltaTime * 15f;
+        // _motorbikeRB.velocity = new Vector3(0f, _motorbikeRB.velocity.y, _moveSpeed);
 
 
-        fWheel.motorTorque = 1000f;
-        bWheel.motorTorque = 1000f;
+        fWheel.motorTorque = motorTorque * boostMultiple;
+        bWheel.motorTorque = motorTorque * boostMultiple;
         //duruma göre hareket ilerleme kodu deðiþebilir
     }
 
@@ -40,8 +47,8 @@ public abstract class Character : MonoBehaviour
 
     internal protected void RotateMotorbike()
     {
-        transform.RotateAround(rotatePoint.position, Vector3.right, 100f*Time.deltaTime);
-        _totalFlipAngle += 100f * Time.deltaTime;
+        transform.RotateAround(rotatePoint.position, Vector3.right, 100f * Time.deltaTime);
+        _totalFlipAngle += 1000f * Time.deltaTime;
 
         if (_totalFlipAngle >= 360f)
         {
@@ -50,7 +57,7 @@ public abstract class Character : MonoBehaviour
         }
     }
 
-    internal protected void SetFlipCount(int count,bool isReset=false)
+    internal protected void SetFlipCount(int count, bool isReset = false)
     {
         _flipCount += count;
 
@@ -60,11 +67,35 @@ public abstract class Character : MonoBehaviour
         {
             //boost açýlacak
             //eðer boost açýksa süresine ekleme yapýlacak
+            StartCoroutine(Boost());
         }
     }
 
     internal protected virtual void EndLevel(bool isWin)
     {
+
+    }
+
+    private IEnumerator Boost()
+    {
+        if (onBoost)
+        {
+            boostTimer += plusBoostTime;
+            yield break;
+        }
+
+        onBoost = true;
+        boostTimer = defaultBoostTime;
+
+        boostMultiple = 2f;
+
+        while (boostTimer > 0)
+        {
+            boostTimer -= Time.deltaTime;
+            yield return null;
+        }
+
+        DOTween.To(() => boostMultiple, x => boostMultiple = x, 1f, 1f);
 
     }
 }
